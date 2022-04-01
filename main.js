5ff888748a8460e9a0183cec947c1ab58fba8607
Fit1245 <87483283+Fit1245@users.noreply.github.com>
2022-04-01 19:43:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+
+}
+function setup() {
+    canvas = createCanvas(400,400);
+    canvas.position(450,130);
+}
+function draw() {
+
+}
+
+function take_snapshot() {
+    save("yourposing_image.png");
+}
